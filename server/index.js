ccfcd6556c230a97818bac0cbf4f4920b6fc16d2
zydplatform <kigandaivan32@gmail.com>
2022-03-22 16:42:27 +0300

    developed API endpoints|routes|Models for Movies App

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
 
 const app = express()
 const apiPort = 3000
+
+db.on('error', console.error.bind(console, 'MongoDB Connection Error:'))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
@@ -11,5 +15,6 @@ app.use(bodyParser.json())
 app.get('/', (req, res)=>{
 	res.send('Hello world')
 })
+app.use('/api', movieRouter)
 
 app.listen(apiPort, ()=>console.log(`Server is running on port ${apiPort}`))
